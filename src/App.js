8c9f0e52dbef75cdc5926ef6aef48123e3010b2c
Kostas Ntallas <kostasdallas2010@hotmail.com>
2024-12-11 23:04:54 +0200

    Use React as front end framework

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <header>
+        <h1>Welcome to My Portfolio</h1>
+      </header>
+      <main>
+        <p>This is my React-powered portfolio!</p>
+      </main>
+    </div>
+  );
+};
+
+export default App;
