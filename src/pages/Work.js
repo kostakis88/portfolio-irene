f48e218a4bd8f31a3fbaf2562de38d61e73138c3
Kostas Ntallas <kostasdallas2010@hotmail.com>
2024-12-13 19:13:33 +0200

    [SCRUM-4] Implement Title Component

--- src/pages/Work.js
+++ src/pages/Work.js
@@ -2,7 +2,21 @@ import React from 'react';
 
 const Work = () => {
     return (
-        <div>Work</div>
+        <div className="section-work">
+            <div className="padding-global">
+                <div className="container-large">
+                    <div className="padding-section-large">
+                        <div className="margin-zero margin-bottom-xxlarge">
+                            <div className="max-width-large">
+                                <div className="margin-zero margin-bottom-small">
+                                    <h2>Work</h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 };
 
