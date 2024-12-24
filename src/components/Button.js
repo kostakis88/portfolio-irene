f0a72b7a8c892b597783b8a83928b372b8ae4b52
Kostas Ntallas <kostasdallas2010@hotmail.com>
2024-12-24 13:40:37 +0200

    [SCRUM-20] Link view all button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Button = () => {
     return (
         <div className="margin-zero margin-top-xsmall">
             <div className="button-group is-center">
-                <a href="/works" className="button is-secondary">View all</a>
+                <Link to={'/works'} className="button is-secondary">View all</Link>
             </div>
         </div>
     );
