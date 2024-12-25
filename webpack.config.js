bcf3fb911ab0ccd56585df9a1b61b47f86e2d5d3
Kostas Ntallas <kostasdallas2010@hotmail.com>
2024-12-25 12:53:00 +0200

    [DEPLOY-1 Revert changes]

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -36,11 +35,6 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './public/index.html',
       favicon: './public/favicon.ico',
-    }),
-    new CopyWebpackPlugin({
-      patterns: [
-        { from: 'public/index.html', to: '404.html' },
-      ],
     }),
     new ReactRefreshWebpackPlugin(),
   ],
