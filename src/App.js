3f051eb698a2b0b0099643831d9f004f88de7e44
Kostas Ntallas <kostasdallas2010@hotmail.com>
2025-01-28 13:18:32 +0200

    [SCRUM-108] Revert Login Page changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
@@ -6,22 +6,8 @@ import Home from './pages/Home';
 import Work from './pages/Work';
 import About from './pages/About';
 import WorkDetails from './pages/WorkDetails';
-import Login from "./pages/Login";
 
 const App = () => {
-
-  const [authenticated, setAuthenticated] = useState(false);
-
-  // Render Login if not authenticated
-  if (!authenticated) {
-    return (
-      <Login
-        onAuthenticated={() => {
-          setAuthenticated(true); // Toggle authentication when the user logs in
-        }}
-      />
-    );
-  }
 
   return (
     <Router>
